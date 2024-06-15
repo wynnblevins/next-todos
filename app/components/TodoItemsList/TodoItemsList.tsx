671103fb8598b7441b241e76b40c3389488f2461
Robert Blevins <robertblevins@roberts-imac.mynetworksettings.com>
2024-06-14 22:17:33 -0400

    styling for disabled add btn, added app margins

--- app/components/TodoItemsList/TodoItemsList.tsx
+++ app/components/TodoItemsList/TodoItemsList.tsx
@@ -92,13 +92,24 @@ const TodoItemsList = (props: Props) => {
           <tr className="border-b border-neutral-200 dark:border-white/10 grid-cols-3">
             <th scope="col" className="px-6 py-4">Todo</th>
             <th>
+            { editing ? (
               <button type="button"  
-                className="float-right mr-8 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                className="cursor-not-allowed float-right mt-3 mr-8 text-white bg-blue-500 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                disabled>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+              </button>
+            ) : (
+              <button type="button"  
+                className="float-right mt-3 mr-8 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 onClick={onAddClick}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                 </svg>
               </button>
+            )}
+              
             </th>
           </tr>
         </thead>
