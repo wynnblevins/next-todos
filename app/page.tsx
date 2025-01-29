60a5e66820f4295c171c178bfb3b2f683818fc1e
Wynn Blevins <wynnblevins@gmail.com>
2025-01-29 15:44:06 -0500

    adding loading spinner

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,17 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { TodoItem } from './api/todos/[id]/route';
 import TodoItemsList from './components/TodoItemsList/TodoItemsList';
+import { LoadingSpinner } from './components/LoadingSpinner/LoadingSpinner';
 
 const TodosListPage = () => {
   const [todos, setTodos] = useState<TodoItem[]>([]);
+  const [dataLoaded, setDataLoaded] = useState<boolean>(false);
   
   useEffect(() => {
     const fetchTodos = async () => {
       const { data } = await axios.get('/api/todos');
       setTodos(data);
+      setDataLoaded(true);
     };
 
     fetchTodos();
@@ -36,12 +39,20 @@ const TodosListPage = () => {
   };
 
   return (
-    <TodoItemsList 
-      onSaveClick={(todoItem) => { onSaveClick(todoItem) }} 
-      onAddClick={(todoItem) => { onAddClick(todoItem) }}
-      onDeleteClick={(todoItem) => { onDeleteClick(todoItem) }} 
-      todos={todos}>
-    </TodoItemsList>
+    <>
+      { dataLoaded ? <>
+        <TodoItemsList 
+          onSaveClick={(todoItem) => { onSaveClick(todoItem) }} 
+          onAddClick={(todoItem) => { onAddClick(todoItem) }}
+          onDeleteClick={(todoItem) => { onDeleteClick(todoItem) }} 
+          todos={todos}>
+        </TodoItemsList>
+      </> : <>
+        <div className="flex justify-center items-center h-screen">
+          <LoadingSpinner></LoadingSpinner>
+        </div>
+      </>}      
+    </>
   )
 }
 
